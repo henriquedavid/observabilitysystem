507c6db597337e570d3c171d2dd8510a7168e576
Henrique David <henriquemed101@gmail.com>
2022-09-09 16:16:47 -0300

    First commit

---
+++ src/main/java/com/henriquedavid/observability/observabilitysys/ObservabilitysysApplication.java
@@ -0,0 +1,13 @@
+package com.henriquedavid.observability.observabilitysys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilitysysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservabilitysysApplication.class, args);
+	}
+
+}
